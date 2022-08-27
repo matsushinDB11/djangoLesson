f5f38a6a792b64de5482042d0163f5eda76e64d2
matsushinDB11 <matsushin35@gmail.com>
2022-08-27 20:04:00 +0900

    fix: read DEBUG from env file

--- djangoLesson/settings.py
+++ djangoLesson/settings.py
@@ -27,7 +27,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
